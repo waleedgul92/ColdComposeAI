c0c43560753f9c95bc38f993f6973decbeb5d0aa
waleedgul92 <hwaleed0035@google.com>
2024-11-09 14:54:56 +0500

    added all files

--- code/chains.py
+++ code/chains.py
@@ -14,6 +14,7 @@ class Chain:
 
     def extract_jobs(self, cleaned_text):
         prompt_extract = PromptTemplate.from_template(
+            
             """
             ### SCRAPED TEXT FROM WEBSITE:
             {page_data}
